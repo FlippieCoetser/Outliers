055437d8307edf8294f0d76011fcc36b3968ea2b
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-04 08:02:24 -0700

    FOUNDATIONS: Boundary.Adjustment.Service() ReturnsList --> FAIL

--- tests/testthat/test-Boundary.Adjustment.Service.R
+++ tests/testthat/test-Boundary.Adjustment.Service.R
@@ -3,3 +3,13 @@ describe("Given Boundary.Adjustment.Service", {
     Boundary.Adjustment.Service |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("Given services <- Boundary.Adjustment.Service()",{
+  it("then services is a list",{
+    # Given
+    services <- Boundary.Adjustment.Service()
+
+    # Then
+    services |> is.list() |> expect_equal(TRUE)
+  })
+})
