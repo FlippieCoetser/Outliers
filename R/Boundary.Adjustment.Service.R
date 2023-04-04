c11b0098be0e1fbf2b464063c514187673bb4fd4
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-04 09:06:50 -0700

    FOUNDATIONS: input |> service[['Upper']]() ReturnsExp(-4*input)IfInputIsEqualOrBiggerZero --> PASS

--- R/Boundary.Adjustment.Service.R
+++ R/Boundary.Adjustment.Service.R
@@ -1,6 +1,14 @@
 Boundary.Adjustment.Service <- \() {
+  skewness <- Skewness.Service()
+
   services <- list()
-  services[['Upper']] <- \() { }
+  services[['Upper']] <- \(medcouple) {
+    adjustments <- list()
+    adjustments[[1]] <- \(medcouple) exp(-3*medcouple)
+    adjustments[[2]] <- \(medcouple) exp(-4*medcouple)
+    
+    adjustments[[(medcouple >= 0) + 1]](medcouple)
+  }
   services[['Lower']] <- \() { }
   return(services)
 }
