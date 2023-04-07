72ff56f8f0bc4c9dd948d808042bb85e0f8156ed
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 06:51:31 -0700

    FOUNDATIONS: boundary.exceptions ContainsNumericException --> PASS

--- R/Boundary.Exceptions.R
+++ R/Boundary.Exceptions.R
@@ -3,5 +3,8 @@ Boundary.Exceptions <- \() {
   exceptions[['NullException']] <- \(invoke) {
     if (invoke) {stop('argument is NULL')}
   }
+  exceptions[['NumericException']] <- \(invoke) {
+
+  }
   return(exceptions)
 }
