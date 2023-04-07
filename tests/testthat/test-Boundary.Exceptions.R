22d3656e70497f1e28c4edf70011c997e8a58594
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 06:45:38 -0700

    FOUNDATIONS: boundary.exceptions ReturnsListOfExceptions --> FAIL

--- tests/testthat/test-Boundary.Exceptions.R
+++ tests/testthat/test-Boundary.Exceptions.R
@@ -3,3 +3,13 @@ describe("Given Boundary.Exceptions", {
     Boundary.Exceptions |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("When boundary.exceptions <- Boundary.Exceptions()",{
+  it("then adjustment.exceptions is a list",{
+    # Given
+    boundary.exceptions <- Boundary.Exceptions()
+
+    # Then
+    boundary.exceptions |> is.list() |> expect_equal(TRUE)
+  })
+})
