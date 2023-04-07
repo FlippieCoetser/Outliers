b06176a5332bbe0303b72e748400adb547793b75
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 07:39:53 -0700

    FOUNDATIONS: input |> outlier.service[['Extract']]() ExtractsOutliersFromInput --> PASS

--- R/Outlier.Service.R
+++ R/Outlier.Service.R
@@ -1,6 +1,10 @@
-Outlier.Service <- \() {
+Outlier.Service <- \(boundary) {
   services <- list()
   services[['Extract']] <- \(sample) {
+    boundary.upper <- sample |> boundary[['Upper']]()
+    boundary.lower <- sample |> boundary[['Lower']]()
+
+    sample[sample < boundary.lower | sample > boundary.upper] 
   }
   return(services)
 }
