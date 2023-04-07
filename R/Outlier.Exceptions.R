81e3e6000da52162e9a7866f3d8132b2199c7118
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 07:57:23 -0700

    FOUNDATIONS: Outlier.Exceptions --> PASS

--- R/Outlier.Exceptions.R
+++ R/Outlier.Exceptions.R
@@ -1,1 +1,10 @@
-Outlier.Exceptions <- \() {}
+Outlier.Exceptions <- \() {
+  exceptions <- list()
+  exceptions[['NullException']] <- \(invoke) {
+    if (invoke) {stop('argument is NULL')}
+  }
+  exceptions[['NumericException']] <- \(invoke) {
+    if (invoke) {stop('argument is not numeric')}
+  }
+  return(exceptions)
+}
