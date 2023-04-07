5a177ff82ce647ee6a2e45102bed882125d19ad7
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 00:47:01 -0700

    FOUNDATIONS: input |> boundary.service[['Upper']]() ReturnsUpperBoundary --> PASS

--- R/Boundary.Service.R
+++ R/Boundary.Service.R
@@ -1,5 +1,14 @@
 Boundary.Service <- \() {
+  quartile   <- Quartile.Service()
+  range      <- IQR.Service() |> IQR.Processing()
+  skewness   <- Skewness.Service()
+  adjustment <- Adjustment.Service()
+
   services <- list()
-  services[['Upper']] <- \() { }
+  services[['Upper']] <- \(sample) {  
+    (sample |> quartile[['third']]()) + 1.5 * 
+    (sample |> range[['IQR']]()) * 
+    (sample |> skewness[['medcouple']]() |> adjustment[['Upper']]())
+  }
   return(services)
 }
