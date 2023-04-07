0914dce10859129978a7664cbad85923f0814e39
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 07:30:56 -0700

    FOUNDATIONS: outlier.services ContainsExtractService --> FAIL

--- tests/testthat/test-Outlier.Service.R
+++ tests/testthat/test-Outlier.Service.R
@@ -5,11 +5,18 @@ describe("Given Outlier.Service", {
 })
 
 describe("Given outlier.services <- Outlier.Service()",{
-  it("then services is a list",{
+  it("then outlier.services is a list",{
     # Given
     outlier.services <- Outlier.Service()
 
     # Then
     outlier.services |> is.list() |> expect_equal(TRUE)
+  })
+  it("then outlier.services contains upper service",{
+    # Given
+    outlier.services <- Outlier.Service()
+
+    # Then
+    outlier.services[['Extract']] |> is.function() |> expect_equal(TRUE)
   })
 })
