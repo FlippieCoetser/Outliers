b1cb9bb9e1bc9467afb4a76000029b6d5d8673d4
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 06:49:51 -0700

    FOUNDATIONS: input |> boundary.exception[['NullException']]() ThrowExceptionIfInputNull --> FAIL

--- tests/testthat/test-Boundary.Exceptions.R
+++ tests/testthat/test-Boundary.Exceptions.R
@@ -20,3 +20,20 @@ describe("When boundary.exceptions <- Boundary.Exceptions()",{
     boundary.exceptions[['NullException']] |> is.function() |> expect_equal(TRUE)
   })
 })
+
+describe("When input |>  boundary.exception[['NullException']]()",{
+  it("then no exception is thrown if input is FALSE",{
+    # Given
+    boundary.exception <- Boundary.Exceptions()
+
+    # Then
+    FALSE |> boundary.exception[['NullException']]() |> expect_no_error()
+  })
+  it("then NULL exception is thrown if input is TRUE",{
+    # Given
+    boundary.exception <- Boundary.Exceptions()
+
+    # Then
+    TRUE |> boundary.exception[['NullException']]() |> expect_error('argument is NULL')
+  })
+})
