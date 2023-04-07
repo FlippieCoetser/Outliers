6ee48e75926e866b2b6fec7ccda86fd1a90993a3
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 00:16:13 -0700

    FOUNDATIONS: Boundary.Service() ReturnsListOfService --> FAIL

--- tests/testthat/test-Boundary.Service.R
+++ tests/testthat/test-Boundary.Service.R
@@ -2,4 +2,14 @@ describe("Given Boundary.Service", {
   it("Exist",{
     Boundary.Service |> is.null() |> expect_equal(FALSE)
   })
-})e
+})
+
+describe("Given services <- Boundary.Service()",{
+  it("then services is a list",{
+    # Given
+    services <- Boundary.Service()
+
+    # Then
+    services |> is.list() |> expect_equal(TRUE)
+  })
+})
