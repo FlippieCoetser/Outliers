2aa0cfd9351d398e14365c49203e3fbc0fbe855e
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 06:05:56 -0700

    FOUNDATIONS: adjustment.exceptions ContainsNumericException --> PASS

--- R/Adjustment.Exceptions.R
+++ R/Adjustment.Exceptions.R
@@ -3,5 +3,8 @@ Adjustment.Exceptions <- \() {
   exceptions[['NullException']]  <- \(invoke) {
     if (invoke) {stop('argument is NULL')}
   }
+  exceptions[['NumericException']] <- \(invoke) {
+    
+  }
   return(exceptions)
 }
