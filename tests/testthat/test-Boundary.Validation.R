2f81ac2055ca81d1775e942060ef93142df4e9a6
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 06:58:41 -0700

    FOUNDATIONS:  boundary.validators ReturnsListOfValidators --> FAIL

--- tests/testthat/test-Boundary.Validation.R
+++ tests/testthat/test-Boundary.Validation.R
@@ -3,3 +3,13 @@ describe("Given Boundary.Validation", {
     Boundary.Validation |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("When boundary.validators <- Boundary.Validation()",{
+  it("then validators is a list",{
+    # Given
+    boundary.validators <- Boundary.Validation()
+
+    # Then
+    boundary.validators |> is.list() |> expect_equal(TRUE)
+  })
+})
