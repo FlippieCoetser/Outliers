42246f699282483c948894b134f6db434acc6b1b
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 05:56:51 -0700

    FOUNDATIONS: adjustment.exceptions ReturnsListOfExceptions --> FAIL

--- tests/testthat/test-Adjustment.Exceptions.R
+++ tests/testthat/test-Adjustment.Exceptions.R
@@ -3,3 +3,13 @@ describe("Given Adjustment.Exceptions", {
     Adjustment.Exceptions |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("When adjustment.exceptions <- Adjustment.Exceptions()",{
+  it("then adjustment.exceptions is a list",{
+    # Given
+    adjustment.exceptions <- Adjustment.Exceptions()
+
+    # Then
+    adjustment.exceptions |> is.list() |> expect_equal(TRUE)
+  })
+})
