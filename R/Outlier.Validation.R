d7aebe7ddee22965f07482f9c7500c4f76e4d24d
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 08:01:20 -0700

    FOUNDATION: Outlier.Validation --> PASS

---
+++ R/Outlier.Validation.R
@@ -0,0 +1,14 @@
+Outlier.Validation <- \() {
+  exception <- Boundary.Exceptions()
+
+  validators <- list()
+  validators[['Exist']] <- \(data) {
+    data |> is.null() |> exception[['NullException']]()
+    return(data)
+  }
+  validators[['IsNumeric']] <- \(data) {
+    data |> is.numeric() |> isFALSE() |> exception[['NumericException']]()
+    return(data)
+  }
+  return(validators)
+}
