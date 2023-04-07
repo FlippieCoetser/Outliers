e84402587394786d60f704ab85ea0ea08970c6ac
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 00:46:54 -0700

    FOUNDATIONS: input |> boundary.service[['Upper']]() ReturnsUpperBoundary --> FAIL

--- tests/testthat/test-Boundary.Service.R
+++ tests/testthat/test-Boundary.Service.R
@@ -20,3 +20,27 @@ describe("Given services <- Boundary.Service()",{
     services[['Upper']] |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("Given input |> service[['Upper']]()",{
+  it("then an upper boundary should be returned",{
+    # Given
+    quartile   <- Quartile.Service()
+    range      <- IQR.Service() |> IQR.Processing()
+    skewness   <- Skewness.Service()
+    adjustment <- Adjustment.Service()
+
+    service <- Boundary.Service()
+    input   <- 1000 |> rnorm(10,5)
+
+    # When
+    expected.boundary <- 
+      (input |> quartile[['third']]()) + 1.5 * 
+      (input |> range[['IQR']]()) * 
+      (input |> skewness[['medcouple']]() |> adjustment[['Upper']]()) 
+
+    actual.boundary <- input |> service[['Upper']]()
+
+    # Then
+    actual.boundary |> expect_equal(expected.boundary)
+  })
+})
