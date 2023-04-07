abadb6e444049fa1bafdf39fa2de39fb80b218d8
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 07:39:46 -0700

    FOUNDATIONS: input |> outlier.service[['Extract']]() ExtractsOutliersFromInput --> FAIL

--- tests/testthat/test-Outlier.Service.R
+++ tests/testthat/test-Outlier.Service.R
@@ -20,3 +20,23 @@ describe("Given outlier.services <- Outlier.Service()",{
     outlier.services[['Extract']] |> is.function() |> expect_equal(TRUE)
   })
 })
+
+describe("When input |> outlier.service[['Extract']]()",{
+  it("then the outlier in input should be returned",{
+    # Given
+    boundary.service <- Boundary.Service()
+
+    outlier.service <- Boundary.Service() |> Outlier.Service()
+    input   <- 1000 |> rnorm(10,5)
+
+    # When
+    boundary.upper <- input |> boundary.service[['Upper']]()
+    boundary.lower <- input |> boundary.service[['Lower']]()
+
+    expected.outliers <- input[input < boundary.lower | input > boundary.upper]
+    actual.outliers   <- input |> outlier.service[['Extract']]()
+
+    # Then
+    actual.outliers |> expect_equal(expected.outliers)
+  })
+})
