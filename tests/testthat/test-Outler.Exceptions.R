3321760811699be8213fefeeb4f5a10219527b36
Flippie Coetser <flippie.coetser@gmail.com>
2023-04-07 07:57:17 -0700

    FOUNDATIONS: Outlier.Exceptions --> FAIL

--- tests/testthat/test-Outler.Exceptions.R
+++ tests/testthat/test-Outler.Exceptions.R
@@ -3,3 +3,61 @@ describe("Given Outlier.Exceptions", {
     Outlier.Exceptions |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("When outlier.exceptions <- Outlier.Exceptions()",{
+  it("then outlier.exceptions is a list",{
+    # Given
+    outlier.exceptions <- Outlier.Exceptions()
+
+    # Then
+    outlier.exceptions |> is.list() |> expect_equal(TRUE)
+  })
+  it("then outlier.exceptions contains NullException exception",{
+    # Given
+    outlier.exceptions <- Outlier.Exceptions()
+
+    # Then
+    outlier.exceptions[['NullException']] |> is.function() |> expect_equal(TRUE)
+  })
+  it("then outlier.exceptions contains NumericException exception",{
+    # Given
+    outlier.exceptions <- Outlier.Exceptions()
+
+    # Then
+    outlier.exceptions[['NumericException']] |> is.function() |> expect_equal(TRUE)
+  })
+})
+
+describe("When input |>  outlier.exception[['NullException']]()",{
+  it("then no exception is thrown if input is FALSE",{
+    # Given
+    outlier.exception <- Outlier.Exceptions()
+
+    # Then
+    FALSE |> outlier.exception[['NullException']]() |> expect_no_error()
+  })
+  it("then NULL exception is thrown if input is TRUE",{
+    # Given
+    outlier.exception <- Outlier.Exceptions()
+
+    # Then
+    TRUE |> outlier.exception[['NullException']]() |> expect_error('argument is NULL')
+  })
+})
+
+describe("When input |> outlier.exception[['NumericException']]()",{
+  it("then no exception is thrown if input is FALSE",{
+    # Given
+    outlier.exception <- Outlier.Exceptions()
+
+    # Then
+    FALSE |> outlier.exception[['NumericException']]() |> expect_no_error()
+  })
+  it("then NULL exception is thrown if input is TRUE",{
+    # Given
+    outlier.exception <- Outlier.Exceptions()
+
+    # Then
+    TRUE |> outlier.exception[['NumericException']]() |> expect_error('argument is not numeric')
+  })
+})
